e795e7194da7357154b0ff29e6b5fa83eff0192e
Lucas Cota <lucasbrunocota@live.com>
2019-04-28 17:06:39 -0300

    Intial commit.
    * Basic BrowserWindow from Electron.
    * Simple splash loading screen.
    * Settings module.

---
+++ source/settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+    WindowWidth: 1280,
+    WindowHeight: 720,
+    DeezerApplicationID: 345002,
+    DeezerUrl: 'https://www.deezer.com/br/',
+    DeezerOAuthUrl: 'https://connect.deezer.com/oauth/auth.php'
+}

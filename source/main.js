c57815600a34ea2d9a4d701caa0f595dea4a99e5
Lucas Cota <lucasbrunocota@live.com>
2019-04-29 00:55:59 -0300

    Moved to webview.

--- source/main.js
+++ source/main.js
@@ -1,37 +1,14 @@
 const settings = require('./settings');
 const { app, BrowserWindow } = require('electron');
 
-var splash;
-
 function createWindow(visibility) {
     return new BrowserWindow({ width: settings.WindowWidth, height: settings.WindowHeight, show: visibility, title: 'DeezerRPC' })
 }
 
-function createSplashWindow() {
-    splash = createWindow(true);
+function createMainWindow() {
+    let window = createWindow(true);
 
-    splash.loadURL(`file://${__dirname}/views/splash.html`)
+    window.loadURL(`file://${__dirname}/views/index.html`);
 }
 
-function createDeezerWindow() {
-    let window = createWindow(false);
-
-    window.loadURL(settings.DeezerUrl);
-
-    window.once('ready-to-show', () => {
-        window.webContents.session.cookies.get({}, cookies => {
-            let arl = cookies.find(key => key.name == "arl");
-
-            if (!arl) {
-                window.loadURL(settings.DeezerOAuthUrl + '?app_id=' + settings.DeezerApplicationID + '&redirect_uri=' + settings.DeezerUrl);
-            }
-
-            splash.close();
-            window.show();
-        });
-    })
-
-    createSplashWindow();
-}
-
-app.on('ready', createDeezerWindow);
+app.on('ready', createMainWindow);
--- source/settings.js
+++ source/settings.js
@@ -2,6 +2,6 @@ module.exports = {
     WindowWidth: 1280,
     WindowHeight: 720,
     DeezerApplicationID: 345002,
-    DeezerUrl: 'https://www.deezer.com/br/',
+    DeezerUrl: 'https://deezer.com/br',
     DeezerOAuthUrl: 'https://connect.deezer.com/oauth/auth.php'
 }

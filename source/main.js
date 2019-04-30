67462cae623ce485a6822a202c39a181e3226ba3
Lucas Cota <lucasbrunocota@live.com>
2019-04-30 11:27:35 -0300

    Removing <webview> and retrieving DOM from webContents.
    * ARL cookie checker is now active again.

--- source/main.js
+++ source/main.js
@@ -7,17 +7,38 @@ function createWindow(visibility) {
         height: settings.WindowHeight,
         show: visibility,
         title: 'DeezerRPC',
-        webPreferences : {
-            nodeIntegration: true,
-            webviewTag: true,
-        }
     });
 }
 
-function createMainWindow() {
-    let window = createWindow(true);
+function createSplashWindow() {
+    splash = createWindow(true);
 
-    window.loadURL(`file://${__dirname}/views/index.html`);
+    splash.loadURL(`file://${__dirname}/views/splash.html`)
+}
+
+function createMainWindow() {
+    let window = createWindow(false);
+
+    window.loadURL(settings.DeezerUrl);
+
+    window.once('ready-to-show', () => {
+        window.webContents.session.cookies.get({}, cookies => {
+            let arl = cookies.find(key => key.name == "arl");
+
+            if (!arl) {
+                window.loadURL(settings.DeezerOAuthUrl + '?app_id=' + settings.DeezerApplicationID + '&redirect_uri=' + settings.DeezerUrl);
+            }
+
+            splash.close();
+            window.show();
+        });
+    })
+
+    window.webContents.executeJavaScript('document.documentElement.innerHTML', function (result) {
+        console.log(result);
+    });
+
+    createSplashWindow();
 }
 
 app.on('ready', createMainWindow);

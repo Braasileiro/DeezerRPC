91aca6c1664d4f5c7f092562b14ff38db61ff89c
Lucas Cota <lucasbrunocota@live.com>
2019-09-11 15:01:37 -0300

    Converting settings to TS.

--- source/settings.ts
+++ source/settings.ts
@@ -1,7 +1,7 @@
-module.exports = {
+export default {
     WindowWidth: 1280,
     WindowHeight: 720,
     DeezerApplicationID: 345002,
-    DeezerUrl: 'https://deezer.com/br',
+    DeezerUrl: 'https://deezer.com',
     DeezerOAuthUrl: 'https://connect.deezer.com/oauth/auth.php'
 }

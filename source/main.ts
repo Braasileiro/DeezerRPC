6f09e81c4b50218980b901dcd2cad6dbd20fa694
Lucas Cota <lucasbrunocota@live.com>
2019-09-13 16:07:29 -0300

    Checking changes on DOM.

--- source/main.ts
+++ source/main.ts
@@ -1,10 +1,6 @@
-import { JSDOM } from "jsdom";
+import path from 'path';
 import Settings from './settings';
 import { app, BrowserWindow } from 'electron';
-
-import Song from './model/Song';
-import Album from './model/Album';
-import Artist from './model/Artist';
 
 var mainWindow: BrowserWindow;
 var splashWindow: BrowserWindow;
@@ -17,6 +13,9 @@ function createWindow(visibility: boolean) {
         height: Settings.WindowHeight,
         show: visibility,
         title: 'DeezerRPC',
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 }
 
@@ -32,57 +31,17 @@ function createMainWindow() {
     mainWindow.loadURL(Settings.DeezerUrl);
 
     mainWindow.once('ready-to-show', () => {
-        mainWindow.webContents.session.cookies.get({name: "arl"}, function(arl) {
+        mainWindow.webContents.session.cookies.get({name: "arl"}, (arl) => {
             if (!arl) {
                 mainWindow.loadURL(Settings.DeezerOAuthUrl + '?app_id=' + Settings.DeezerApplicationID + '&redirect_uri=' + Settings.DeezerUrl);
             }
 
-            splashWindow.close();
             mainWindow.show();
+            splashWindow.close();
         });
     });
 
-    setTimeout(getCurrentSong, 7000);
-
     createSplashWindow();
-}
-
-async function getCurrentSong(): Promise<Song> {
-    let response = await retrieveDocument('document.querySelector("div.marquee-content").innerHTML');
-
-    let selector = response.querySelectorAll("a.track-link");
-
-    let album = new Album("", retrieveAttribute(selector[0], "href"));
-
-    let artists: Artist[] = [];
-
-    selector.forEach((value, index) => {
-        if (index == 0) return;
-
-        artists.push(new Artist(retrieveTextContent(value), retrieveAttribute(value, "href")));
-    });
-
-    let song =  new Song(retrieveTextContent(selector[0]), album, artists);
-
-    console.log(song.toString());
-
-    return song;
-}
-
-
-// Selector
-async function retrieveDocument(query: string): Promise<Document> {
-    let result = await mainWindow.webContents.executeJavaScript(query);
-
-    return new JSDOM(result).window.document;
-}
-
-function retrieveAttribute(element: Element, attr: string): string {
-    return element.getAttribute(attr) || "";
-}
-
-function retrieveTextContent(element: Element): string {
-    return element.textContent || "";
 }
 
 

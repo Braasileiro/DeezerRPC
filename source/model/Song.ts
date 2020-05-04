cb6f29127b2accdd1be68cc2bb7f81f018f261d7
Lucas Cota <lucasbrunocota@live.com>
2020-05-04 17:09:22 -0300

    LIstening indicator.

--- source/model/Song.ts
+++ source/model/Song.ts
@@ -2,10 +2,12 @@ export default class Song {
     name: string;
     artist: string;
     time: number;
+    listening: boolean;
 
-    constructor(name: string, artist: string, time: number) {
+    constructor(name: string, artist: string, time: number, listening: boolean = true) {
         this.name = name;
         this.artist = artist;
         this.time = time;
+        this.listening = listening;
     }
 }

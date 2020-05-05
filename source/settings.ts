73c8970889f916beeaff095bb70ebc2971fa8929
Lucas Cota <lucasbrunocota@live.com>
2020-05-05 10:18:25 -0300

    Updating packages.

--- source/settings.ts
+++ source/settings.ts
@@ -1,8 +1,6 @@
 export default {
     WindowWidth: 1280,
     WindowHeight: 720,
-    DeezerApplicationID: 345002,
     DeezerUrl: 'https://www.deezer.com/br/login',
-    DeezerOAuthUrl: 'https://connect.deezer.com/oauth/auth.php',
     DiscordClientID: '623154118122274839'
 }

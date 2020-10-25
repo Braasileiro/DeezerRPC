236cd29493c1a21614eb04803ef76897fd8b2dbd
Lucas Cota <lucasbrunocota@live.com>
2020-10-25 00:12:12 -0300

    ClientID is now private.

--- source/settings.ts
+++ source/settings.ts
@@ -2,5 +2,5 @@ export default {
     WindowWidth: 1280,
     WindowHeight: 720,
     DeezerUrl: 'https://www.deezer.com/login',
-    DiscordClientID: '623154118122274839'
+    DiscordClientID: ''
 }

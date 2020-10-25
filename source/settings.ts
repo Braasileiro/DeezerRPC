7f730fc62eeb9b60c935331ce2f112b62d57b0a6
Lucas Cota <lucasbrunocota@live.com>
2020-10-24 23:14:31 -0300

    Fixes and new features. Version 1.0.2!
    * Fixed tray icon issue;
    * Fixed tray version web link url;
    * Added icons for play/pause, next and previous on tray menu;
    * User preferences are now stored;
    * Default url to Deezer without forced regional locale.
    * Updated user agents;
    * Updated modules.

--- source/settings.ts
+++ source/settings.ts
@@ -1,6 +1,6 @@
 export default {
     WindowWidth: 1280,
     WindowHeight: 720,
-    DeezerUrl: 'https://www.deezer.com/br/login',
+    DeezerUrl: 'https://www.deezer.com/login',
     DiscordClientID: '623154118122274839'
 }

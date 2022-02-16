86360b8ef9d9e0e1dab1e4b527d240c666e15422
Lucas Cota <lucasbrunocota@live.com>
2022-02-16 16:35:27 -0300

    Refactoring.

--- src/playlist/playlist.ts
+++ src/playlist/playlist.ts
@@ -1,13 +1,11 @@
-const contextMenuScript = `
-window.oncontextmenu = (e) => {
-    if (e.target.closest('#page_naboo_playlist')) {
-        const songRow = e.target.closest('[role="row"]');
-        songRow.querySelector('.popper-wrapper button').click();
-        return false;
-    }
-}
-`;
-
 export function registerContextMenu() {
-    __mainWindow.webContents.executeJavaScript(contextMenuScript);
+    __mainWindow.webContents.executeJavaScript(
+        `window.oncontextmenu = (event) => {
+            if (event.target.closest('#page_naboo_playlist')) {
+                const songRow = event.target.closest('[role="row"]');
+                songRow.querySelector('.popper-wrapper button').click();
+                return false;
+            }
+        };0`
+    );
 }

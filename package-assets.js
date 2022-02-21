70a2e899b2476a80d20590b488e25e209d1cad82
Lucas Cota <lucasbrunocota@live.com>
2022-02-21 19:20:29 -0300

    Refactoring.

--- package-assets.js
+++ package-assets.js
@@ -5,5 +5,6 @@ shell.cp('-R', './src/web', './build/');
 
 if (!fs.existsSync('./build/assets')){
     shell.mkdir('./build/assets');
+    shell.cp('-R', './assets/icon', './build/assets/icon');
     shell.cp('-R', './assets/tray', './build/assets/tray');
 }

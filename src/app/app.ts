17f1b1ed837b13550eaeaa2a8cdfb21e4fd6df60
Lucas Cota <lucasbrunocota@live.com>
2022-11-03 14:30:40 -0300

    Public ClientId.

--- src/app/app.ts
+++ src/app/app.ts
@@ -20,7 +20,7 @@ export const APP = {
         windowWidth: 1280,
         windowHeight: 720,
         deezerUrl: 'https://www.deezer.com/login',
-        discordClientID: ''
+        discordClientID: '769757579177623563'
     },
     preferences: {
         closeToTray: 'closeToTray',
